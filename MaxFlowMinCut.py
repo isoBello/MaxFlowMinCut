4b2123761ac452f35a20fa78f4bc15cc46493885
isoBello <isabellamfreitas2@gmail.com>
2021-02-03 13:27:03 -0300

    Add apply dfs method

--- MaxFlowMinCut.py
+++ MaxFlowMinCut.py
@@ -2,51 +2,43 @@
 # -*- coding: utf-8 -*-
 from collections import defaultdict
 
-graph = None
-
-
-class Graph:
-    def __init__(self, vertices):
-        # Using a list of adjacency to represent the graph
-        self.vertices = set(range(1, vertices + 1))
-        self.edges = defaultdict(list)
-        self.weights = {}
-
-    def add_edge(self, u, v):
-        # Storing the weights with a dict. The key is a -> b. Value is the weight himself
-        self.edges[u].append(v)
-        self.edges[v].append(u)
-
-        self.weights[(u, v)] = 1
-        self.weights[(v, u)] = 1
-
 
 def create_graph():
     num_vertices, num_edges = (map(int, input().split(" ")))
-    global graph
-    graph = Graph(num_vertices)
+    vertices = set(range(1, num_vertices + 1))
+    edges = defaultdict(list)
+    weights = {}
 
-    while num_edges > 0:
+    while True:
         try:
             u, v = (map(int, input().split(" ")))
-            graph.add_edge(u, v)
-            num_edges -= 1
+            add_edge(u, v, edges, weights)
         except EOFError:
             break
+
+    return vertices, edges, weights
+
+
+# Storing the weights with a dict. The key is a -> b. Value is the weight himself
+def add_edge(u, v, edges, weights):
+    edges[u].append(v)
+    edges[v].append(u)
+
+    weights[(u, v)] = 1
+    weights[(v, u)] = 1
 
 
 # This is based on the implementation of the CLRS book.
 # The ford-fulkerson algorithm needs to run BFS to find the augmentation path.
-def BFS(source, sink, parent):
-    global graph
+def BFS(source, sink, parent, edges, weights):
     visited = [False] * (sink + 1)
     stack = [source]
     visited[source] = True
 
     while stack:
         u = stack.pop(0)
-        for v in graph.edges[u]:
-            if not visited[v] and graph.weights[u, v] > 0:
+        for v in edges[u]:
+            if not visited[v] and weights[u, v] > 0:
                 visited[v] = True
                 parent[v] = u
                 stack.append(v)
@@ -55,18 +47,17 @@ def BFS(source, sink, parent):
     return True if visited[sink] else False
 
 
-# The DFS method is used to search in traversal of the graph
-def DFS(source, visited):
-    global graph
+# The DFS method is used to search for the path in residual graph
+def DFS(source, visited, num_vertices, edges):
     visited[source] = True
 
-    for v in range(len(graph.vertices)):
-        if v in graph.edges[s] and not visited[v]:
-            DFS(v, visited)
+    for v in range(num_vertices):
+        if v in edges[source] and not visited[v]:
+            DFS(v, visited, num_vertices, edges)
 
 
-def find_minCut(source, sink):
-    global graph
+def find_minCut(source, sink, vertices, edges, weights):
+    num_vertices = len(vertices)
     parent = [-1] * (sink + 1)
     maximum_flow = 0
 
@@ -74,7 +65,7 @@ def find_minCut(source, sink):
         flow = 99999
         vertex = sink
         while vertex != source:
-            flow = min(flow, graph.weights[parent[vertex], vertex])
+            flow = min(flow, weights[parent[vertex], vertex])
             vertex = parent[vertex]
 
         maximum_flow += flow
@@ -82,24 +73,28 @@ def find_minCut(source, sink):
 
         while vertex != source:
             u = parent[vertex]
-            graph.weights[u, vertex] -= flow
-            graph.weights[vertex, u] += flow
+            weights[u, vertex] -= flow
+            weights[vertex, u] += flow
             vertex = parent[vertex]
 
-    visited = len(graph.vertices) * [False]
-    DFS(source, visited)
+    apply_dfs(source, num_vertices, edges, weights)
+
+
+# In this method, we run DFS in the final residual graph founded by BFS.
+# We use this to find the edges used by the graph. This represents the cut.
+def apply_dfs(source, num_vertices, edges, weights):
+    visited = num_vertices * [False]
+    DFS(source, visited, num_vertices, edges)
 
     answer = 0
-    for u in range(len(graph.vertices)):
-        for v in graph.edges[u]:
-            if graph.weights[u, v] == 0 and visited[u]:
+    for u in range(num_vertices):
+        for v in edges[u]:
+            if weights[u, v] == 0 and visited[u]:
                 answer += 1
 
     print(answer)
 
 
 if __name__ == "__main__":
-    create_graph()
-    s = 1
-    t = len(graph.vertices)
-    find_minCut(s, t)
+    vertices, edges, weights = create_graph()
+    find_minCut(1, len(vertices), vertices, edges, weights)
