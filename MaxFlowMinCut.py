b93bbcfdd6573a859d5d3fb4a40cf2e1d2baaf53
isoBello <isabellamfreitas2@gmail.com>
2021-02-05 11:46:23 -0300

    Create Ford-Fulkerson method

--- MaxFlowMinCut.py
+++ MaxFlowMinCut.py
@@ -73,6 +73,7 @@ def create_bipartite(color, edges, source, sink):
             B.add(i)
     A.remove(0)
 
+
     weights = {}
     wedges = defaultdict(list)
     netflow = [source]
@@ -81,83 +82,115 @@ def create_bipartite(color, edges, source, sink):
         add_weighted_edge(source, u, wedges, weights)
         netflow.append(u)
         for v in edges[u]:
-            add_weighted_edge(u, v, wedges, weights)
+            if v not in A:
+                add_weighted_edge(u, v, wedges, weights)
     for u in B:
         add_weighted_edge(u, sink, wedges, weights)
         netflow.append(u)
     netflow.append(sink)
 
-# # This is based on the implementation of the CLRS book.
-# # The ford-fulkerson algorithm needs to run BFS to find the augmentation path.
-# def BFS(source, sink, parent, edges, weights):
-#     visited = [False] * (source + 1)
-#     stack = [source]
-#     visited[source] = True
-#
-#     while stack:
-#         u = stack.pop(0)
-#         for v in edges[u]:
-#             if not visited[v] and weights[u, v] > 0:
-#                 visited[v] = True
-#                 parent[v] = u
-#                 stack.append(v)
-#
-#     # This 'return' means that we have a path between source and sink, founded by BFS
-#     return True if visited[sink] else False
-#
-#
-# # The DFS method is used to search for the path in residual graph
+    return netflow, wedges, weights, A, B
+
+
+# This is based on the implementation of the CLRS book.
+# The ford-fulkerson algorithm needs to run BFS to find the augmentation path.
+def BFS(source, sink, parent, edges, weights, paths, count):
+    visited = [False] * (sink + 1)
+    stack = [source]
+    visited[source] = True
+    path = {(0, 0): -1}
+
+    BFS_path = []
+
+    while stack:
+        u = stack.pop(0)
+
+        for v in edges[u]:
+            if not visited[v] and weights[u, v] > 0:
+                visited[v] = True
+                parent[v] = u
+                stack.append(v)
+                path[(u, v)] = 1
+    # Construct the path in BFS. Just to see if BFS is working.
+    # Does the same has parent list but elegantly.
+    node = sink
+
+    if visited[sink]:
+        BFS_path.append(node)
+        while node != source:
+            next_node = [u for u, v in path.keys() if v == node][0]
+            node = next_node
+            BFS_path.append(node)
+        BFS_path.reverse()
+        paths[count] = BFS_path
+    # This 'return' means that we have a path between source and sink, founded by BFS
+    return True if visited[sink] else False
+
+
+# The DFS method is used to search for the path in residual graph
 # def DFS(source, visited, num_vertices, edges):
 #     visited[source] = True
 #
 #     for v in range(num_vertices):
 #         if v in edges[source] and not visited[v]:
 #             DFS(v, visited, num_vertices, edges)
-#
-#
-# # We run BFS to find the residual graph's in the original graph
-# # After that, we remove the problem of antiparallel edges by incresing one and decresing the other
-# def find_minCut(source, sink, vertices, edges, weights):
-#     num_vertices = len(vertices)
-#     parent = [-1] * (source + 1)
-#     maximum_flow = 0
-#
-#     while BFS(source, sink, parent, edges, weights):
-#         flow = float('infinity')
-#         vertex = sink
-#         while vertex != source:
-#             flow = min(flow, weights[parent[vertex], vertex])
-#             vertex = parent[vertex]
-#
-#         maximum_flow += flow
-#         vertex = sink
-#
-#         while vertex != source:
-#             u = parent[vertex]
-#             weights[u, vertex] -= flow
-#             weights[vertex, u] += flow
-#             vertex = parent[vertex]
-#
-#     apply_dfs(source, num_vertices, edges, weights)
-#
-#
+
+
+# We run BFS to find the residual graph's in the original graph
+# After that, we remove the problem of antiparallel edges by incresing one and decresing the other
+def find_minCut(vertices, edges, weights, A, B, source, sink):
+    num_vertices = len(vertices)
+    parent = [-1] * (sink + 1)
+    maximum_flow = 0
+    count = 0
+    paths = {count: []}
+
+    while BFS(source, sink, parent, edges, weights, paths, count):
+        flow = float('infinity')
+        v = sink
+        while v != source:
+            flow = min(flow, weights[parent[v], v])
+            v = parent[v]
+
+        maximum_flow += flow
+        v = sink
+        while v != source:
+            u = parent[v]
+            weights[u, v] -= flow
+            v = parent[v]
+
+        count += 1
+
+    print(paths)
+    # apply_dfs(source, sink, paths)
+
+
 # # In this method, we run DFS in the final residual graph founded by BFS.
 # # We use this to find the edges used by the graph. This represents the cut.
-# def apply_dfs(source, num_vertices, edges, weights):
-#     visited = (num_vertices + 1) * [False]
-#     DFS(source, visited, num_vertices, edges)
-#
-#     answer = 0
-#     for u in range(num_vertices):
-#         for v in edges[u]:
-#             if weights[u, v] == 0 and visited[u]:
-#                 answer += 1
-#
-#     print(answer)
+def apply_dfs(source, sink, paths):
+    answer = 0
+
+    # print(paths)
+    for k, v in paths.items():
+        print(k)
+        print(v)
+        # v.remove(source)
+        # v.remove(sink)
+        # print(v)
+    # visited = num_vertices * [False]
+    # DFS(source, visited, num_vertices, edges)
+    #
+    # answer = 0
+    # for u in range(num_vertices):
+    #     for v in edges[u]:
+    #         if weights[u, v] == 0 and visited[u] and u in A and v in B:
+    #             answer += 1
+    #
+    # print(answer)
+
+
 if __name__ == "__main__":
     vertices, edges = create_graph()
     colors = coloring_graph(vertices, edges)
-    create_bipartite(colors, edges, source=0, sink=len(vertices) + 1)
-
-    # isBipartite(vertices, edges)
-    # find_minCut(len(vertices), 1, vertices, edges, weights)
+    netflow, wedges, weights, A, B = create_bipartite(colors, edges, source=0, sink=len(vertices) + 1)
+    find_minCut(netflow, wedges, weights, A, B, source=0, sink=len(vertices) + 1)
